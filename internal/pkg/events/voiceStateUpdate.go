faed11285a2b6f4a62c27bb4e54ce4f137cfaeef
Ben Whitley <dev@purplg.com>
2020-05-12 22:15:23 -0500

    Removed unused go

--- internal/pkg/events/voiceStateUpdate.go
+++ internal/pkg/events/voiceStateUpdate.go
@@ -1,13 +1,11 @@
 package events
 
 import (
-	"fmt"
-
 	"github.com/bwmarrin/discordgo"
-	"purplg.com/memberchannels/internal/pkg/database"
 )
 
 func (config *Events) VoiceStateUpdate(session *discordgo.Session, event *discordgo.VoiceStateUpdate) {
+
 	log := config.Log.WithField("GuildID", event.GuildID)
 
 	guildDB := config.DB.AsGuild(event.GuildID)
@@ -53,43 +51,4 @@ func (config *Events) VoiceStateUpdate(session *discordgo.Session, event *discor
 	if err := widget.Sweep(); err != nil {
 		log.WithError(err).Errorln("Deleting empty channels")
 	}
-}
-
-func (config *Events) onUserConnect(session *discordgo.Session, guildDB database.GuildDatabase, user *discordgo.User) error {
-	userChannelName := guildDB.UserChannelName(user.ID)
-	if userChannelName == "" {
-		userChannelName = fmt.Sprintf("%s's channel", user.Username)
-	}
-	listeningChannelID := guildDB.ChannelID()
-	listeningChannelName := guildDB.ChannelName()
-
-	// Reconfigure listening channel to a user channel
-	channel, err := session.ChannelEditComplex(listeningChannelID, &discordgo.ChannelEdit{
-		Name: userChannelName,
-		PermissionOverwrites: []*discordgo.PermissionOverwrite{
-			{
-				ID:    user.ID,
-				Type:  "member",
-				Allow: 16,
-				Deny:  0,
-			},
-		},
-	})
-	if err != nil {
-		return err
-	}
-	guildDB.SetUserChannel(user.ID, channel.ID, userChannelName)
-
-	// Create new listening channel
-	channel, err = session.GuildChannelCreateComplex(guildDB.GuildID(), discordgo.GuildChannelCreateData{
-		Name:     listeningChannelName,
-		Type:     discordgo.ChannelTypeGuildVoice,
-		ParentID: guildDB.CategoryID(),
-	})
-	if err != nil {
-		return err
-	}
-
-	guildDB.SetChannelID(channel.ID)
-	return nil
 }
